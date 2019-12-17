1647f9b469b5dd639d0237223513d8a9444d3b46
Damian Reyes <reyesmezadamian@gmail.com>
2019-12-17 02:52:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,4 +31,6 @@ monopoly = {
 	   names:{reading_railroad: {}, pennsylvania_railroad: {},
 	   b_and_o_railroad: {}, shortline_railroad: {}},
 	   }}
+monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+monopoly
 end
