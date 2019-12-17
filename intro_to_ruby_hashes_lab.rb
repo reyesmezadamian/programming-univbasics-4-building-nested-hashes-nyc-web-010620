7c7eb25c8620cad099c061e3e42e11301a76fb53
Damian Reyes <reyesmezadamian@gmail.com>
2019-12-17 02:40:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_third_tier
 	monopoly = {
 	  railroads:{
 	   pieces: 4,
-	   rent_in_dollars: {one_piece_owned: 25 , key2: {}, key3: {}, key4: {}},
+	   rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, key3: {}, key4: {}},
 	   names:{key1: {}, key2: {}, key3: {}, key4: {}},
 	   }}
 end
